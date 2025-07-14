72e112097a33cd44f593f57f341c14c8232a4d20
Starrick2001 <lebuidihoa257@gmail.com>
2025-07-14 23:01:57 +0700

    feat: Generate commit messages using Gemini API
    The commit introduces the functionality to generate commit messages using the Gemini API. It reads the staged changes using `git diff --cached`, sends the diff to the Gemini API, and then prints the generated commit message. It also adds `.gitignore` file to ignore `main` and `commit-craft` binaries.

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 
 	"google.golang.org/genai"
 )
@@ -17,21 +18,27 @@ func Int32(v int32) *int32 {
 func main() {
 	ctx := context.Background()
 	apiKey := os.Getenv("COMMIT_CRAFT_GEMINI_KEY")
+	diff, err := exec.Command("git", "diff", "--cached").Output()
+	if err != nil {
+		log.Fatalln(err)
+	}
 	client, err := genai.NewClient(ctx, &genai.ClientConfig{APIKey: apiKey})
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln(err)
 	}
 
 	result, _ := client.Models.GenerateContent(
 		ctx,
 		"gemini-2.0-flash",
-		genai.Text("Explain how AI works in a few words"),
+		genai.Text("You are an expert at creating a git commit message for a set of changes. Return the generated title commit message. Here is a diff of changes we need a commit message for: "+string(diff)),
+		// genai.Text("You are an expert at creating a git commit message for a set of changes. Return a git commit command line with generated commit message. Here is a diff of changes we need a commit message for: "+string(diff)),
 		&genai.GenerateContentConfig{
-			ThinkingConfig: &genai.ThinkingConfig{
-				ThinkingBudget: Int32(0), // Disables thinking
-			},
+			// ThinkingConfig: &genai.ThinkingConfig{
+			// ThinkingBudget: Int32(0), // Disables thinking
+			// },
 		},
 	)
 
-	fmt.Println(result.Text())
+	fmt.Println("Generated commit msg: ", result.Text())
+	exec.Command("git", "commit", "-m", result.Text()).Output()
 }
