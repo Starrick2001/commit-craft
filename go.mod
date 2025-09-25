b9be250af315808c9c3bbae57aced569fd1c60a0
Starrick2001 <lebuidihoa257@gmail.com>
2025-09-25 12:12:53 +0700

    temp

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module commit-craft
 go 1.24.2
 
 require (
+	github.com/charmbracelet/bubbletea v1.3.4
 	github.com/charmbracelet/huh v0.7.0
 	google.golang.org/genai v1.15.0
 )
@@ -15,7 +16,6 @@ require (
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/catppuccin/go v0.3.0 // indirect
 	github.com/charmbracelet/bubbles v0.21.0 // indirect
-	github.com/charmbracelet/bubbletea v1.3.4 // indirect
 	github.com/charmbracelet/colorprofile v0.2.3-0.20250311203215-f60798e515dc // indirect
 	github.com/charmbracelet/lipgloss v1.1.0 // indirect
 	github.com/charmbracelet/x/ansi v0.8.0 // indirect
--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
 
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/huh"
 	"google.golang.org/genai"
 )
@@ -23,6 +25,65 @@ const (
 	StateQuit
 	StateModify
 )
+
+type Model struct {
+	state       int
+	description string
+	config      Config
+	form        *huh.Form
+}
+
+func initialModel(form *huh.Form, config Config) Model {
+	return Model{
+		state:       StateCommit,
+		description: "",
+		form:        form,
+		config:      config,
+	}
+}
+
+func (m Model) Init() tea.Cmd {
+	return m.form.Init()
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c":
+			return m, tea.Interrupt
+		case "esc", "q":
+			return m, tea.Quit
+		}
+	}
+
+	var cmds []tea.Cmd
+
+	// Process the form
+	form, cmd := m.form.Update(msg)
+	if f, ok := form.(*huh.Form); ok {
+		m.form = f
+		cmds = append(cmds, cmd)
+	}
+
+	if m.form.State == huh.StateCompleted {
+		// Quit when the form is done.
+		cmds = append(cmds, tea.Quit)
+	}
+
+	return m, tea.Batch(cmds...)
+}
+
+func (m Model) View() string {
+	switch m.form.State {
+	case huh.StateCompleted:
+		class := m.form.GetString("class")
+		level := m.form.GetInt("level")
+		return fmt.Sprintf("You selected: %s, Lvl. %d", class, level)
+	default:
+		return m.form.View()
+	}
+}
 
 func showOutputScreen(msg string) {
 	log.Printf("Generated commit msg: '%s' \n", msg)
@@ -59,16 +120,21 @@ func showOutputScreen(msg string) {
 
 func main() {
 	ctx := context.Background()
-	apiKey, isEnvApiKeyFound := os.LookupEnv("COMMIT_CRAFT_GEMINI_KEY")
+	apiKey, isEnvApiKeyFound := os.LookupEnv("COMMIT_CRAFT_GEMINI_KEi11")
 	if !isEnvApiKeyFound {
 		form := huh.NewForm(
 			huh.NewGroup(
-				huh.NewInput().Title("Can not find Gemini Api key in your env, what is your Gemini Api Key?").Value(&apiKey),
+				huh.NewInput().Title("Can not find Gemini Api key in your env, what is your Gemini Api Key?").Key("apiKey"),
 			),
 		)
-		if err := form.Run(); err != nil {
-			log.Fatal(err)
+		p := tea.NewProgram(initialModel(form, Config{}))
+		if _, err := p.Run(); err != nil {
+			log.Fatalf("failed to run program:%v", err)
+			os.Exit(1)
 		}
+		// if err := form.Run(); err != nil {
+		// 	log.Fatal(err)
+		// }
 	}
 	diff, err := exec.Command("git", "diff", "--cached").Output()
 	if err != nil {
@@ -86,7 +152,7 @@ func main() {
 					huh.NewOption("gemini-2.0-flash", "gemini-2.0-flash"),
 					huh.NewOption("gemini-2.5-flash", "gemini-2.5-flash")).
 				Value(&config.model)),
-	)
+	).WithProgramOptions(tea.WithAltScreen())
 	if err := form.Run(); err != nil {
 		log.Fatal(err)
 	}
@@ -94,6 +160,7 @@ func main() {
 	if err != nil {
 		log.Fatalln("Failed to create Gemini client:%w", err)
 	}
+	var thinkingBudget int32 = 0
 
 	result, err := client.Models.GenerateContent(
 		ctx,
@@ -101,9 +168,9 @@ func main() {
 		genai.Text("You are an expert at creating a git commit message for a set of changes. Return the generated title commit message. Here is a diff of changes we need a commit message for: "+string(diff)),
 		// genai.Text("You are an expert at creating a git commit message for a set of changes. Return a git commit command line with generated commit message. Here is a diff of changes we need a commit message for: "+string(diff)),
 		&genai.GenerateContentConfig{
-			// ThinkingConfig: &genai.ThinkingConfig{
-			// ThinkingBudget: Int32(0), // Disables thinking
-			// },
+			ThinkingConfig: &genai.ThinkingConfig{
+				ThinkingBudget: &thinkingBudget, // Disables thinking
+			},
 		},
 	)
 	if err != nil {
