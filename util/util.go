0e7b6c403aef33d8ea4f3e77809d4379871a98ea
Starrick2001 <lebuidihoa257@gmail.com>
2026-02-15 05:36:49 +0700

    chore: update configuration and refactor provider logic

--- util/util.go
+++ util/util.go
@@ -1,6 +1,5 @@
 package util
 
 const (
-	GeneralPrompt        = "You are an expert at creating a git commit message for a set of changes. Here is a diff of changes we need a commit message for: "
-	GEMINI_DEFAULT_MODEL = "models/gemini-flash-latest"
+	GeneralPrompt = "You are an expert at creating a git commit message for a set of changes. Here is a diff of changes we need a commit message for: "
 )
